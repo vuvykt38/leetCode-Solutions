0daf8d4d5d11b023dd692649ce4dd0c7577f5ce6
Vy <vuvykt38@gmail.com>
2020-04-14 22:24:48 +0700

    adding solution for “Last Stone Weight”

---
+++ last-stone-weight.rb
@@ -0,0 +1,33 @@
+# Last Stone Weight
+# We have a collection of stones, each stone has a positive integer weight.
+
+# Each turn, we choose the two heaviest stones and smash them together.  Suppose the stones have weights x and y with x <= y.  The result of this smash is:
+
+# If x == y, both stones are totally destroyed;
+# If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
+# At the end, there is at most 1 stone left.  Return the weight of this stone (or 0 if there are no stones left.)
+
+
+
+# Example 1:
+
+# Input: [2,7,4,1,8,1]
+# Output: 1
+# Explanation:
+# We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+# we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+# we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+# we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of last stone.
+
+def last_stone_weight(stones)
+  return stones.first if stones.size <= 1
+  stones.sort!
+  last = stones.pop
+  second_last = stones.pop
+  stones << last - second_last
+  last_stone_weight(stones)
+end
+p last_stone_weight([2,7,4,1,8,1])
+p last_stone_weight([1])
+p last_stone_weight([1,3])
+p last_stone_weight([2,2])
