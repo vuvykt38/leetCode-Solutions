da2a4001e9c33081ec575bbf2b95ae5edd4de165
Vy <vuvykt38@gmail.com>
2020-09-27 12:35:19 +0700

    adding solution for “448.Find All Numbers Disappeared in an Array”

---
+++ find-all-numbers-disappeared-in-an-array.rb
@@ -0,0 +1,35 @@
+# 448. Find All Numbers Disappeared in an Array
+# Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+# Find all the elements of [1, n] inclusive that do not appear in this array.
+
+# Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+# Example:
+
+# Input:
+# [4,3,2,7,8,2,3,1]
+
+# Output:
+# [5,6]
+
+def find_disappeared_numbers(nums)
+  nums.sort!
+  i = 1
+  n = nums.size + 1
+  count = 0
+  while i < n
+    if !nums.include?(i)
+      nums << i
+      count += 1
+    end
+  i += 1
+  end
+  p nums
+  nums.last(count)
+end
+
+p find_disappeared_numbers([4,3,2,7,8,2,3,1])
+# p find_disappeared_numbers([3,2,7,7,2,3,1,1])
+# p find_disappeared_numbers([3,2,7,7,2,3,1,1,4])
+# p find_disappeared_numbers([1,1])
